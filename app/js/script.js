97edad4549963402510d80a926e06945d70cbabf
Nikita Savchenko <Nikita Savchenko>
2016-10-21 16:50:13 +0300

    deal

--- app/js/script.js
+++ app/js/script.js
@@ -264,4 +264,23 @@ $(document).ready(function(){
         });
     });
 
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav',
+    });
+    $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: false,
+        centerMode: true,
+        focusOnSelect: true,
+        vertical: true,
+        arrows: false,
+        verticalSwiping: true
+
+    });
 });
